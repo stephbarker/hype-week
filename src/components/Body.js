ef0a23899cca06e7a169cb1f512b5aa9eb7d1958
Steph Barker <sbarkerdrums@gmail.com>
2021-02-10 14:30:35 -0800

    change to Hear Your Hype, and removes unused files

--- src/components/Body.js
+++ src/components/Body.js
@@ -6,29 +6,13 @@ import { useDataLayerValue } from "../DataLayer";
 
 function Body({ token }) {
     const [{ discover_weekly }] = useDataLayerValue()
-    // const playPlaylist = () => {
-    //     dispatch({
-    //         type: "SET_PLAYING",
-    //         playing: true,
-    //       })
-    //       dispatch({
-    //         type: "SET_PLAY_URI",
-    //         play_uri: "spotify:playlist:37i9dQZEVXcKjGV31cVGoB",
-    //       })
-    //       spotify.getMyCurrentPlayingTrack().then((r) => {
-    //         dispatch({
-    //           type: "SET_ITEM",
-    //           item: r.item,
-    //         })
-    //       })
-    //     }
-
+ 
     return (
         <div className="body">
             <div className="body_info">
                 <img className='img' src={discover_weekly?.images[0]?.url} alt="" />
                 <div className="body__infoText">
-                    <h2> Hear Your Hype Week!</h2>
+                    <h2> Hear Your Hype!</h2>
                     <p>{discover_weekly?.description}</p>
                 </div>
             </div>
@@ -54,16 +38,6 @@ function Body({ token }) {
                 }}
               />
               </div>
-                {/* <div className="body__icons">
-                    <PlayCircleFilled className="body__shuffle" onClick={playPlaylist} />
-                    <Favorite fontSize="large" />
-                    <MoreHoriz />
-                </div> */}
-                {/* <div className="body__songs">
-                {discover_weekly?.tracks.items.map((item) => (
-                    <SongRow track={item.track} />
-                ))}
-                 </div>     */}
             </div>
     );
 }
